cf8c473f78c4c8eb7f83e5fbb5e109e49833e296
Levi Freire <levifreire7@gmail.com>
2022-07-21 20:11:56 -0300

    Insercao de usuario com POST

--- src/main/java/com/levifreire/springbootmongodb/resources/UserResource.java
+++ src/main/java/com/levifreire/springbootmongodb/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.levifreire.springbootmongodb.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.levifreire.springbootmongodb.domain.User;
 import com.levifreire.springbootmongodb.dto.UserDTO;
@@ -35,6 +39,14 @@ public class UserResource {
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
+
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
+		User obj = service.fromDto(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 }
