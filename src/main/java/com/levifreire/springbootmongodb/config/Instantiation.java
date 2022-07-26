9f295e4c7ea7401c1074019ecf3cc9399b92965b
Levi Freire <levifreire7@gmail.com>
2022-07-26 15:48:53 -0300

    Projecao dos dados do autor com DTO

--- src/main/java/com/levifreire/springbootmongodb/config/Instantiation.java
+++ src/main/java/com/levifreire/springbootmongodb/config/Instantiation.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.levifreire.springbootmongodb.domain.Post;
 import com.levifreire.springbootmongodb.domain.User;
+import com.levifreire.springbootmongodb.dto.AuthorDTO;
 import com.levifreire.springbootmongodb.repository.PostRepository;
 import com.levifreire.springbootmongodb.repository.UserRepository;
 
@@ -36,11 +37,12 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraćos!",
-				maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
-
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraćos!",
+				new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
 		postRepository.saveAll(Arrays.asList(post1, post2));
 
 	}
